e9bce38f1d607efc6581683ebc3454e5cb02fa22
Eden Edry <edenedry99@gmail.com>
2022-12-14 12:14:08 +0200

    add logout

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,21 @@
-from flask import Blueprint, render_template,request,flash
+from flask import Blueprint, render_template,request,flash,redirect
+# from .models import User
+# from . import db
+from flask_login import login_user, login_required, logout_user,current_user
 auth = Blueprint('auth',__name__)
 
 @auth.route('/Login' , methods=['GET' , 'POST'])
 def Login():
-    return render_template("login.html" , text="צוות רפואי / מטופל")
+    return render_template("login.html" , text="Medical staff / patient ")
 @auth.route ('/Logout')
+@login_required
 def Logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
+
 @auth.route('/Sign_up', methods=['GET' , 'POST'])
 def Sign_up():
+
     if request.method == 'POST':
         choose = request.form.get('choose')
         ID = request.form.get('ID')
@@ -16,19 +23,19 @@ def Sign_up():
         name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        if choose != "צוות רפואי" or choose !="מטופל":
-            flash("הכנס מטופל / צוות רפואי בלבד", category='error')
-        elif len(ID) != 9:
-            flash("אורך תעודת הזהות צריך להיות בן 9 ספרות", category='error')
+        # if choose != "Medical staff" or choose !="patient":
+        #      flash("Enter patient/ medical staff", category='error')
+        if len(ID) != 9:
+            flash("ID card length should be 9 digits", category='error')
 
-        elif len(email)<4:
-            flash("אורך כתובת המייל צריך להיות לפחות 5 ", category='error')
-        elif len(name) < 2:
-            flash("נא להכניס שם בעל יותר משתי אותיות", category='error')
-
-        elif password1 != password2:
-            flash("הסיסמאות לא זהות", category='error')
+        # elif len(email)<4:
+        #     flash("The length of the email address should be at least 5 characters", category='error')
+        # elif len(name) < 2:
+        #     flash("Enter a name with at least two letters", category='error')
+        #
+        # elif password1 != password2:
+        #     flash("Passwords are not the same", category='error')
 
         else:
-            flash( "המשתמש נוצר בהצלחה!" , category='secces')
+            flash( "User created successfully!" , category='secces')
     return render_template("sign_up.html")
