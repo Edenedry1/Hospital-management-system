4ea2316b9525152ea0d8f6020c4e23db7c78dc89
noaro2 <117273562+noaro2@users.noreply.github.com>
2022-12-16 01:45:36 +0200

    add user stories

--- website/models.py
+++ website/models.py
@@ -4,18 +4,19 @@ from sqlalchemy.sql import func
 '''
 #create database models
 '''
+
 class Note(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_ID = db.Column(db.Integer, db.ForeignKey('user.ID'))
 
-class User(db.Model,UserMixin):
+class User(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     Name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    # notes = db.relationship('Note')
 
 
 
