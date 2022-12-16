4a9722c6ba04b139f9bc0ddc58475c4b022afe39
RonHodayaSela <117276382+RonHodayaSela@users.noreply.github.com>
2022-12-16 14:38:38 +0200

    Add files via upload

--- tast.py
+++ tast.py
@@ -1,76 +1,87 @@
-# def test_Sign_up_redirect(client):
-#     response = client.get('/ID')
-#     # Check that there was one redirect response.
-#     assert len(response.history) == 0
-#     # Check that the second request was to the index page.
-#     assert response.request.path == '/ID'
-#
-#
-# def test_Sign_up_redirect(client):
-#     response = client.get('/email')
-#     # Check that there was one redirect response.
-#     assert len(response.history) == 0
-#     # Check that the second request was to the index page.
-#     assert response.request.path == '/email'
-#
-#
-# def test_Sign_up_redirect(client):
-#     response = client.get('/name')
-#     # Check that there was one redirect response.
-#     assert len(response.history) == 0
-#     # Check that the second request was to the index page.
-#     assert response.request.path == '/name'
-#
-#
-# # def test_Sign_up_redirect(client):
-# #     response = client.get('/password1')
-# #     # Check that there was one redirect response.
-# #     assert len(response.history) == 0
-# #     # Check that the second request was to the index page.
-# #     assert response.request.path == '/password2'
-#
-#
-# def test_logout_redirect(client):
-#     response = client.get('/Logout')
-#     # Check that there was one redirect response.
-#     assert len(response.history) == 0
-#     # Check that the second request was to the index page.
-#     assert response.request.path == '/Logout'
-#
-#     import pytest
-#     from website import create_app
-#
-#     @pytest.fixture()
-#     def app():
-#         app = create_app()
-#         app.config.update({
-#             "TESTING": True,
-#         })
-#
-#         # other setup can go here
-#
-#         yield app
-#
-#         # clean up / reset resources here
-#
-#     @pytest.fixture()
-#     def client(app):
-#         return app.test_client()
-#
-#     @pytest.fixture()
-#     def runner(app):
-#         return app.test_cli_runner()
-#
-#     def test_login_redirect(client):
-#         response = client.get('/Login')
-#         # Check that there was one redirect response.
-#         assert len(response.history) == 0
-#         # Check that the second request was to the index page.
-#         assert response.request.path == '/Login'
-#
-#     def test_Sign_up_redirect(client):
-#         response = client.get('/POST')
-#         # Check that there was one redirect response.
-#         assert len(response.history) == 0
-#         # Check that the second request was to the index page.
-#         assert response.request.path == '/POST'
+import pytest
+from website import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    # other setup can go here
+    yield app
+    # clean up / reset resources here
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+def test_login_redirect(client):
+    response = client.get("/login")
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == "/login"
+
+def test_Sign_up_redirect(client):
+    response = client.get('/ID')
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == '/ID'
+
+def test_Sign_up_redirect(client):
+    response = client.get('/name')
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == '/name'
+    #
+    #
+def test_Sign_up_redirect(client):
+    response = client.get('/password1')
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == '/password2'
+    #
+    #
+def test_logout_redirect(client):
+    response = client.get('/Logout')
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == '/Logout'
+    #
+
+
+def test_login_redirect(client):
+    response = client.get('/Login')
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == '/Login'
+
+
+def test_Sign_up_redirect(client):
+    response = client.get('/POST')
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == '/POST'
+
+
+def test_Sign_up_redirect(client):
+    response = client.get('/email')
+    # Check that there was one redirect response.
+    assert len(response.history) == 0
+    # Check that the second request was to the index page.
+    assert response.request.path == '/email'
+import website
+
+def test_Login(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
+    expected = {"login.html"}
+    assert expected == website.loads(res.get_data(as_text=True))
