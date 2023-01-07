b1bf38508d459707d9f40d6eeed2054738d92309
Edenedry1 <117269968+Edenedry1@users.noreply.github.com>
2023-01-07 18:39:55 +0200

    Add files via upload

--- website/__init__.py
+++ website/__init__.py
@@ -3,11 +3,13 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-DB_NAME="database.db"
+DB_NAME = "database.db"
+
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'NoaEdenRonShoham'
-    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
@@ -16,7 +18,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User,Note
+    from .models import User, Note
     with app.app_context():
         db.create_all()
 
